22bcb8e63b8b19befa27160f1ad567f4e619c984
CRD-digital <carloshazard@gmail.com>
2025-11-16 11:30:35 -0300

    Create types.ts

---
+++ Modulo_03/CineTrack/src/script/ts/types.ts
@@ -0,0 +1,23 @@
+// Enums
+export type Genero =
+  | "Ação"
+  | "Comédia"
+  | "Drama"
+  | "Ficção Cientifica"
+  | "Terror"
+  | "Romance"
+  | "Animação"
+  | "Aventura";
+
+export type Status =
+  | "Quero ver"
+  | "Assistindo"
+  | "Concluído";
+
+export type TipoTitulo = "Filme" | "Série";
+
+export type Plataforma =
+  | "Netflix"
+  | "Prime Video"
+  | "Disney+"
+  | "HBO Max";
