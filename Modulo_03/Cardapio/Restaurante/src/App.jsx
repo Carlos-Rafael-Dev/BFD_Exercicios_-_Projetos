4091d1d505a95e46b02f2f8c5a4ca0d654e08125
CRD-digital <carloshazard@gmail.com>
2025-12-03 16:52:46 -0300

    Update App.jsx

--- Modulo_03/Cardapio/Restaurante/src/App.jsx
+++ Modulo_03/Cardapio/Restaurante/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   
   return (
     <div className='container'>
-      <Banner/>
+      <Banner />
 
       <Categorias 
         numCategoriaSelecionada={numCategoriaSelecionada}
